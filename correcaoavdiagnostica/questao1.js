013a85f482502ee31a55495ecc01c292802951e3
Jaíza Freire <99412036+vanysai@users.noreply.github.com>
2023-02-11 11:33:45 -0300

    Create questao1.js

---
+++ correcaoavdiagnostica/questao1.js
@@ -0,0 +1,13 @@
+let a = 10
+let b = 20
+let c = a * b
+let d = (a + b + c)
+
+
+let media = (d + a + b + c) / 4
+console.log('Questão 1')
+console.log('a: ' + a)
+console.log('b: ' + b)
+console.log('c: ' + c)
+console.log('d: ' + d)
+console.log('media: '  + media)
